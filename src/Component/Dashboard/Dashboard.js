56e54183b55bb28c2ea3ea68a62c2112fc604f22
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-04 21:56:20 +0530

    radar chart

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -49,7 +49,44 @@ const Dashboard = () => {
         },
       ];
 
-
+      const radarData = [
+        {
+          subject: 'Math',
+          A: 120,
+          B: 110,
+          fullMark: 150,
+        },
+        {
+          subject: 'Chinese',
+          A: 98,
+          B: 130,
+          fullMark: 150,
+        },
+        {
+          subject: 'English',
+          A: 86,
+          B: 130,
+          fullMark: 150,
+        },
+        {
+          subject: 'Geography',
+          A: 99,
+          B: 100,
+          fullMark: 150,
+        },
+        {
+          subject: 'Physics',
+          A: 85,
+          B: 90,
+          fullMark: 150,
+        },
+        {
+          subject: 'History',
+          A: 65,
+          B: 85,
+          fullMark: 150,
+        },
+      ];
       
   return (
     <div>
@@ -65,7 +102,15 @@ const Dashboard = () => {
  
           </Col>
           <Col>
-
+          <h3>This is Radarchart</h3>
+          <ResponsiveContainer width="100%" height="100%">
+          <RadarChart cx="50%" cy="50%" outerRadius="80%" data={radarData}>
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis />
+            <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+          </RadarChart>
+        </ResponsiveContainer>
           </Col>
         </Row>
       </Container>
