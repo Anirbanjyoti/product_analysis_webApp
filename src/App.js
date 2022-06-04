a55ab75a70aa062e052f9a90d32c560639d12e8d
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-06-04 13:18:55 +0530

    react bootstrap instlation and route stup and ui ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
+import Blog from './Component/Blog/Blog';
+import Dashboard from './Component/Dashboard/Dashboard';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import Reviews from './Component/Reviews/Reviews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+    <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/Reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/Dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/Blog' element={<Blog></Blog>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import display_img from "../../Capture.PNG";
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <Container>
+                    <Row className='display-section'>
+                        <Col sm={12} md={8} className="phone-des">
+                        <h1>Your Best Iphone</h1>
+                        <p>Why buy Apple iPhone 13 Pro Max
+
+ 
+                <ul>
+                    <li>  Huge 1TB Storage helps you to store your files easily</li>
+                    <li>iOS 15 ensures smooth usage</li>
+                    <li>iOS 15 ensures smooth usage</li>
+                    <li>LiDAR Scanner enhances its efficiency</li>
+                    <li>A15 Bionic Chip to install and run multiple apps</li>
+                    <li>6GB RAM ensures efficient performance</li>
+                    <li>Triple Rear Camera with a single front camera takes stunning pictures</li>
+                </ul>
+                
+                
+                
+
+                Get ready to capture cinematic shots as the Apple iPhone 13 Pro Max features a new Cinematic mode. This smartphone takes your photography experience to a new height. It comes with an amazing combination of 12 MP (telephoto camera), 12 MP (wide camera), and 12 MP (ultra-wide camera). Snap memorable moments in clear detail with the 12 MP wide camera. This phone is made for low light. The Wide camera adds a wider aperture and our largest sensor yet and it leverages the LiDAR Scanner for Night mode portraits. With its redesigned lens and powerful autofocus system, the new Ultra-Wide camera can focus at just 2 centimeters making even the</p> 
+                                <Button className='demo' variant="primary" size="sm">View Demo</Button>
+                </Col>
+                        <Col md={4} sm={12}>
+                            <Image className="phone-image" src={display_img} alt='image'></Image>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
+                            <div className="all_reviews">
+                                <Button variant="primary" size="lg">View All Reviews</Button>
+                                </div>
+                        </Col>
+                    </Row>
+                    </Container>
+        </div>
+    );
+};
+
+export default Home;
